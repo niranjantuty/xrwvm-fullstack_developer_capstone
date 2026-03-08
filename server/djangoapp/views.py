8a75821b543f96d5bebc038186e51deaa0659a33
Niranjan Kumar S <47939895+niranjantuty@users.noreply.github.com>
2026-03-08 16:49:07 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,11 +1,11 @@
 import json
 import logging
 
-from django.contrib import messages
+# from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-from django.shortcuts import get_object_or_404, redirect, render
+from django.http import JsonResponse
+# from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import CarMake, CarModel
