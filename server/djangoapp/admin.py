6d3c317d0b1a053834729a00b1481caa44be7e60
“Niranjan” <niranjantuty@gmail.com>
2026-03-08 03:56:59 -0400

    Add M3.2 djangomodels

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,6 +1,11 @@
 # from django.contrib import admin
 # from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
+# Registering models with their respective admins
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
 # Register your models here.
 
@@ -11,3 +16,18 @@
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+# Inline class for CarModel to be displayed within CarMake admin
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+
+# Admin class for CarMake with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name', 'country', 'founded_year')
+    search_fields = ['name', 'country']
+
+# Admin class for CarModel
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year', 'dealer_id')
+    list_filter = ('car_make', 'type', 'year')
+    search_fields = ['name', 'car_make__name']
